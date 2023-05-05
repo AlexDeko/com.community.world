1b625233f91b33d560abc4e20243356e71fa4dd0
Alexander Deynenko <alex.deko.69@gmail.com>
2023-05-05 21:34:33 +0300

    Initial commit

---
+++ src/main/kotlin/com/community/world/Application.kt
@@ -0,0 +1,19 @@
+package com.community.world
+
+import com.community.world.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureSockets()
+    configureSecurity()
+    configureRouting()
+}
